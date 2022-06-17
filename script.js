d7613733c96f871e96ed65b7a94938bd0e541f24
manibdeveloper <main@email.com>
2022-06-17 18:21:48 -0500

    Push Commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
-  .then(reg => alert('Registro de SW exitoso \n' + reg))
-  .catch(err => alert('Error al tratar de registrar el sw\n' + err))
+  .then(reg => console.log('Registro de SW exitoso \n' + reg))
+  .catch(err => console.log('Error al tratar de registrar el sw\n' + err))
 }
