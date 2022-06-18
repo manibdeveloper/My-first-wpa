7a3bd1d7a724eda9343d9e76b50c83b8101db190
manibdeveloper <main@email.com>
2022-06-18 01:21:15 -0500

    Push Commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 }*/
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
-  //.then(reg => console.log('Registro de SW exitoso \n' + reg))
-  //.catch(err => console.log('Error al tratar de registrar el sw\n' + err))
+  .then(reg => console.log('Registro de SW exitoso \n' + reg, reg))
+  .catch(err => console.log('Error al tratar de registrar el sw\n' + err))
 }
+console.log(8)
--- sw.js
+++ sw.js
@@ -1,93 +1,39 @@
-; //asignar un nombre y versión al cache
-const CACHE_NAME = 'my_first_wpa_v1.1',
-  urlsToCache = [
-    './',
-    './style.css',
-    './script.js',
-    './img/icon_v_1024.png',
-    './img/favicon.png'
-  ]
-
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
-/*
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
-      })
-      .catch(err => alert('Falló registro de cache \n' + err))
-  )
-})*/
+//asignar un nombre y versión al cache
+const cache_version = 'mi_cache_v1.2'
+const urlsToCache = [
+  './',
+  './style.css',
+  './script.js',
+  './img/icon_v_1024.png',
+  './img/favicon.png'
+]
 
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
   e.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(urlsToCache);
-  })());
-});
-
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
-/*
-self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
-
-  e.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
-            }
-          })
-        )
-      })
-      // Le indica al SW activar el cache actual
-      .then(() => self.clients.claim())
-  )
+    const cache = await caches.open(cache_version)
+    await cache.addAll(urlsToCache)
+    ; self.skipWaiting()
+  })())
 })
-*/
+
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then((keyList) => {
     return Promise.all(keyList.map((key) => {
-      if (key === CACHE_NAME) { return; }
-      return caches.delete(key);
+      if (key !== cache_version) return caches.delete(key)
     }))
-  }));
-});
-
-//cuando el navegador recupera una url
-/*
-self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
-  e.respondWith(
-    caches.match(e.request)
-    .then(res => {
-      if (res) {
-        //recuperar del cache
-        return res
-      }
-      //recuperar de la petición a la url
-      return fetch(e.request)
-    })
-  )
-})*/
+    .then(() => self.clients.claim())
+    ;;
+  }))
+})
 
 self.addEventListener('fetch', (e) => {
   e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) { return r; }
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
-});
-/**/
+    const res = await caches.match(e.request)
+    if (res) return res
+    
+    const response = await fetch(e.request)
+    // const cache = await caches.open(cacheName);
+    // cache.put(e.request, response.clone());
+    return response
+  })())
+})
