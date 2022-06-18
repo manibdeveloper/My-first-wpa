27298e42cfa6ad9d397e0795ecf691605a437075
manibdeveloper <main@email.com>
2022-06-18 01:04:01 -0500

    Push Commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+/*console.logi = function (e) {
+  alert(e)
+}*/
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
-  .then(reg => console.log('Registro de SW exitoso \n' + reg))
-  .catch(err => console.log('Error al tratar de registrar el sw\n' + err))
+  //.then(reg => console.log('Registro de SW exitoso \n' + reg))
+  //.catch(err => console.log('Error al tratar de registrar el sw\n' + err))
 }
--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@ const CACHE_NAME = 'my_first_wpa_v1.1',
   ]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+/*
 self.addEventListener('install', e => {
-  alert("install")
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -19,9 +19,19 @@ self.addEventListener('install', e => {
       })
       .catch(err => alert('Falló registro de cache \n' + err))
   )
-})
+})*/
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(urlsToCache);
+  })());
+});
 
 //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+/*
 self.addEventListener('activate', e => {
   const cacheWhitelist = [CACHE_NAME]
 
@@ -41,20 +51,43 @@ self.addEventListener('activate', e => {
       .then(() => self.clients.claim())
   )
 })
+*/
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === CACHE_NAME) { return; }
+      return caches.delete(key);
+    }))
+  }));
+});
 
 //cuando el navegador recupera una url
+/*
 self.addEventListener('fetch', e => {
   //Responder ya sea con el objeto en caché o continuar y buscar la url real
   e.respondWith(
     caches.match(e.request)
-      .then(res => {
-        if (res) {
-          //recuperar del cache
-          return res
-        }
-        //recuperar de la petición a la url
-        return fetch(e.request)
-      })
+    .then(res => {
+      if (res) {
+        //recuperar del cache
+        return res
+      }
+      //recuperar de la petición a la url
+      return fetch(e.request)
+    })
   )
-})
+})*/
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
 /**/
